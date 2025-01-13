ccb2569708b66b432f337699bd1810773a0fa1b4
Nikita Vasilev <nv3212@gmail.com>
2025-01-13 14:27:47 +0100

    Implement support for Swift 5.7 to 5.10

---
+++ Package@swift-5.8.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "transitions",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "Transitions", targets: ["Transitions"]),
+    ],
+    targets: [
+        .target(name: "Transitions"),
+        .testTarget(name: "TransitionsTests", dependencies: ["Transitions"]),
+    ]
+)
